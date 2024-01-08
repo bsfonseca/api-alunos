df61eec28b27c204dffcb3fa1f502221f92bac82
bsfonseca <bs.fonseca12@gmail.com>
2024-01-08 17:45:10 -0300

    Rotas para avaliações, alunos e login

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,32 @@
 import express, { Request, Response } from "express";
-import { Aluno } from "./models/aluno.model";
-import { PrismaClient } from "@prisma/client";
+
 import { AlunoController } from "./controllers/aluno.controller";
+
+import { AvaliacaoController } from "./controllers/avaliacao.controller";
+import { AuthController } from "./controllers/auth.controller";
 
 const app = express();
 app.use(express.json());
 
+// Instâncias
 const alunoController = new AlunoController();
+const avaliacaoController = new AvaliacaoController();
+const authController = new AuthController();
 
+// Rotas de aluno
 app.post("/aluno", alunoController.criarAluno);
-
 app.get("/aluno/:id", alunoController.obterAluno);
-
+app.get("/aluno", alunoController.listarAlunos);
 app.delete("/aluno/:id", alunoController.deletarAluno);
+app.put("/aluno/:id", alunoController.atualizarAluno);
+
+// Rotas de avaliação
+app.post("/aluno/:id/avaliacao", avaliacaoController.criarAvaliacao);
+app.get("/aluno/:id/avaliacao", avaliacaoController.listarAvaliacoes);
+app.put("/aluno/:id/avaliacao/:idAvaliacao", avaliacaoController.atualizarAvaliacao);
+
+//Rotas de autenticação
+app.post("/login", authController.login);
 
 app.listen(3333, () => {
     console.log("Api está rodando");
