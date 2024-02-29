c60a267e6637f2a4ccec5442c08fd65b01e42799
bsfonseca <bs.fonseca12@gmail.com>
2024-02-29 10:18:16 -0300

    feat: validação para tipo do aluno na avaliação

--- src/routes/avaliacao.routes.ts
+++ src/routes/avaliacao.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { AvaliacaoController } from "../controllers/avaliacao.controller";
 import { validaLoginMiddleware } from "../middlewares/login.middleware";
+import { validarAcessoCriarAvaliacaoMid } from "../middlewares/autorizacao.middleware";
+import { validaAcessoPuteDelteMid } from "../middlewares/autPuteDelete.middleware";
 
 export function avaliacaoRoutes() {
     const router = Router({
@@ -9,9 +11,13 @@ export function avaliacaoRoutes() {
 
     const avaliacaoController = new AvaliacaoController();
 
-    router.post("/", [validaLoginMiddleware], avaliacaoController.criarAvaliacao);
+    router.post("/", [validaLoginMiddleware, validarAcessoCriarAvaliacaoMid], avaliacaoController.criarAvaliacao);
     router.get("/", [validaLoginMiddleware], avaliacaoController.listarAvaliacoes);
-    router.put("/:idAvaliacao", [validaLoginMiddleware], avaliacaoController.atualizarAvaliacao);
+    router.put(
+        "/:idAvaliacao",
+        [validaLoginMiddleware, validaAcessoPuteDelteMid],
+        avaliacaoController.atualizarAvaliacao
+    );
 
     return router;
 }
